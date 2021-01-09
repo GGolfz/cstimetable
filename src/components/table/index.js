ce8bd8f65729a548cda93c262899642842ed98ed
GGolfz <wisarutgolf11@gmail.com>
2021-01-10 01:28:49 +0700

    [FIX] CSS

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -3,15 +3,14 @@ import { useEffect, useState } from "preact/hooks";
 import { day } from "../../config/day";
 import { time } from "../../config/time";
 import Subject from "../subject/index";
-const Table = ({subject}) => {
+const Table = ({ subject }) => {
   const [table, setTable] = useState([]);
   const [head, setHead] = useState([]);
   const [left, setLeft] = useState([]);
   useEffect(() => {
-      createTable();
+    createTable();
   }, [subject]);
   const createTable = async () => {
-
     let days = day.map((el) => el.substring(0, 3).toUpperCase());
     let times = [];
     time.map((el, index) => {
@@ -22,37 +21,37 @@ const Table = ({subject}) => {
     setHead(["", ...times]);
     setLeft([...days]);
     let tableTemp = [];
-    for(let i = 0 ;i<day.length;i++){
-        let tableTempDay = [];
-        for(let j = 0;j<time.length;j++){
-            let data = await getSubject(day[i],time[j]);
-            tableTempDay.push({value:data.value,width:data.width})
-            j+= data.width-1;
-        }
-        tableTemp.push(tableTempDay);
+    for (let i = 0; i < day.length; i++) {
+      let tableTempDay = [];
+      for (let j = 0; j < time.length; j++) {
+        let data = await getSubject(day[i], time[j]);
+        tableTempDay.push({ value: data.value, width: data.width });
+        j += data.width - 1;
+      }
+      tableTemp.push(tableTempDay);
     }
     setTable(tableTemp);
-    console.log(tableTemp)
-  }
-  const getSubject = async (d,ti) => {
-    for(let s of subject){
-        if(s.day == d){
-            let index = await time.findIndex(t => t == ti);
-            let start = await time.findIndex(t => t == s.startTime)
-            let end = await time.findIndex(t => t == s.endTime)
-            if(index >= start && index < end){
-                let value ={
-                    time: `${s.startTime} - ${s.endTime}`,
-                    room: s.room,
-                    subject: s.subject
-                }
-                console.log(s,d,ti,index,start,end)
-                return {value,width:end-start}
-            }
+    console.log(tableTemp);
+  };
+  const getSubject = async (d, ti) => {
+    for (let s of subject) {
+      if (s.day == d) {
+        let index = await time.findIndex((t) => t == ti);
+        let start = await time.findIndex((t) => t == s.startTime);
+        let end = await time.findIndex((t) => t == s.endTime);
+        if (index >= start && index < end) {
+          let value = {
+            time: `${s.startTime} - ${s.endTime}`,
+            room: s.room,
+            subject: s.subject,
+          };
+          console.log(s, d, ti, index, start, end);
+          return { value, width: end - start };
         }
+      }
     }
-    return {value:'',width:1}
-  }
+    return { value: "", width: 1 };
+  };
 
   const generateTable = () => {
     return (
@@ -60,7 +59,7 @@ const Table = ({subject}) => {
         <div className="table-row-head">
           {head.map((v, i) => {
             return (
-              <div className="table-head-col" style={{ width: `${200 / 26}%` }}>
+              <div className="table-head-col" style={{ width: `${200 / 24}%` }}>
                 {v}
               </div>
             );
@@ -69,38 +68,44 @@ const Table = ({subject}) => {
         {table.map((row, index) => {
           return (
             <Fragment>
-            <div className="table-row">
-              <div className="table-row-left" style={{ width: `${200 / 26}%` }}>
-                {left[index]}
-              </div>
-              {row.map((col, ind) => {
-                return (
-                  <div
-                    className="table-col"
-                    style={{ width: `${(col.width * 100) / 26}%` }}
-                  >
-                    {col.value == "" ? (
-                      col.value
-                    ) : (
-                      <Subject value={col.value} />
-                    )}
-                  </div>
-                );
-              })}
-              <div className="linerow">
-              {
-                  time.map((col,index)=> {
-                    if(index == 0){
-                      return <div className="linerowhead" style={{width:`${200/26}%`}}> </div>
-                    }
-                    if(index % 2 == 0 ){
-                    return (
-                      <div className="line" style={{width:`${200/26}%`}}/>
-                    )}
-                  })
-                }
+              <div className="table-row">
+                <div
+                  className="table-row-left"
+                  style={{ width: `${200 / 24}%` }}
+                >
+                  {left[index]}
                 </div>
-            </div>
+                {row.map((col, ind) => {
+                  return (
+                    <div
+                      className="table-col"
+                      style={{ width: `${(col.width * 100) / 24}%` }}
+                    >
+                      {col.value == "" ? (
+                        col.value
+                      ) : (
+                        <Subject value={col.value} />
+                      )}
+                    </div>
+                  );
+                })}
+                <div className="linerow">
+                  <div
+                    className="linerowhead"
+                    style={{ width: `${200 / 24}%` }}
+                  />
+                  {time.map((col, index) => {
+                    if (index % 2 == 0) {
+                      return (
+                        <div
+                          className="line"
+                          style={{ width: `${200 / 24}%` }}
+                        />
+                      );
+                    }
+                  })}
+                </div>
+              </div>
             </Fragment>
           );
         })}
