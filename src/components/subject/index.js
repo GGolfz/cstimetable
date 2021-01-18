e316b9ff931885de02f24d13621518b09075d4e2
GGolfz <wisarutgolf11@gmail.com>
2021-01-18 08:27:54 +0700

    [ADD] Mobile Responsive

--- src/components/subject/index.js
+++ src/components/subject/index.js
@@ -7,6 +7,7 @@ const Subject = ({value}) => {
             <div className="subject-content">
                 <div className="subject-time">{value.time} <span className="subject-room">@{value.room}</span></div>
                 <div className="subject-name">{value.subject}</div>
+                <div className="subject-name mini">{value.subject.split(' ').slice(0,3).join(' ')}</div>
             </div>
         </div>
     )
