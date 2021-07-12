d6944c6ea2f2366f8a97384605f46ed0d47d48d9
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-12 13:54:39 +0700

    Edit plan

--- src/config/plan.js
+++ src/config/plan.js
@@ -1,21 +1,30 @@
 exports.plan = [
-    {
-        value:'y1',
-        text: 'Year 1'
-    },{
-        value:'y1-fast',
-        text: 'Year 1 Fast Track'
-    },{
-        value:'y2',
-        text: 'Year 2'
-    },{
-        value:'y2-fast',
-        text: 'Year 2 Fast Track'
-    },{
-        value:'y3',
-        text: 'Year 3'
-    },{
-        value:'y4',
-        text: 'Year 4'
-    }
-];
+	{
+		value: 'y1',
+		text: 'Year 1',
+	},
+	// {
+	//     value:'y1-fast',
+	//     text: 'Year 1 Fast Track'
+	// },
+	{
+		value: 'y2',
+		text: 'Year 2',
+	},
+	{
+		value: 'y2-fast',
+		text: 'Year 2 Fast Track',
+	},
+	{
+		value: 'y3',
+		text: 'Year 3',
+	},
+	{
+		value: 'y3-fast',
+		text: 'Year 3 Fast Track',
+	},
+	{
+		value: 'y4',
+		text: 'Year 4',
+	},
+]
